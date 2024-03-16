31f9b5052765dc2e924c138d7c782e5fe18737c2
Jorge Robledo <robledo.11@hotmail.com>
2024-03-15 21:06:45 -0500

    Remover todos los archivos del seguimiento excepto la carpeta 'utilities'

--- setup.py
+++
@@ -1,4 +0,0 @@
-# Sobre el script o notebook que hará llamada al módulo pyfunctions
-import sys
-sys.path.append(r'C:\\Users\\Jorge Robledo\\Documents\\Python\\Scripts_and_Notebooks\\Artificial Intelligence\\pyfunctions')
-from pyfunctions import *
